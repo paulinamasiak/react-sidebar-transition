0b4bce001c4c4227c5f2019e63f8f1c7aa10cd6b
Paulina Masiak <paula.masiak@gmail.com>
2018-09-13 15:03:16 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Backdrop from './components/Backdrop';
+import Sidebar from './components/Sidebar';
 import BtnIcon from './components/BtnIcon';
 
 import './App.css';
@@ -12,11 +14,18 @@ class App extends Component {
     };
 
     this.openSidebar = this.openSidebar.bind(this);
+    this.closeSidebar = this.closeSidebar.bind(this);
   }
 
   openSidebar() {
     this.setState({
       isSidebarOpen: true,
+    });
+  }
+
+  closeSidebar() {
+    this.setState({
+      isSidebarOpen: false,
     });
   }
 
@@ -28,7 +37,16 @@ class App extends Component {
         <header className="App__header App__header--primary">
           <BtnIcon icon="menu" onClick={this.openSidebar} />
         </header>
-        <main className="App__main" />
+        <main className="App__main">
+          <Backdrop show={isSidebarOpen} onClick={this.closeSidebar}>
+            <BtnIcon
+              className="App__backdrop-close"
+              icon="close"
+              onClick={this.closeSidebar}
+            />
+          </Backdrop>
+          <Sidebar show={isSidebarOpen} />
+        </main>
       </div>
     );
   }
