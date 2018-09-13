73dad519a5ff8ef01d4b6ad80708d6f3c91e8844
Paulina Masiak <paula.masiak@gmail.com>
2018-09-13 16:45:36 +0200

    Update Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import Drawer from '../Drawer';
 import Button from '../Button';
+import Icon from '../Icon';
+import List from '../List';
 import routes from '../../routes';
 
 import './Sidebar.css';
 
+const ACTIVE_ROUTE_ID = 1;
+
+const renderListItem = (route) => {
+  const cssClasses = classNames('Sidebar__link', {
+    'Sidebar__link--active': route.id === ACTIVE_ROUTE_ID,
+  });
+
+  return (
+    <a className={cssClasses}>
+      <Icon className="Sidebar__link-icon" name={route.icon} size={20} />
+      <span>{route.name}</span>
+    </a>
+  );
+};
+
 const Sidebar = (props) => (
   <Drawer show={props.show}>
     <div className="Sidebar">
-      <div className="Sidebar__body" />
+      <nav className="Sidebar__body">
+        <List items={routes} itemRenderer={renderListItem} />
+      </nav>
       <div className="Sidebar__footer">
         <Button color="secondary" block>
-          Sing out
+          Sign out
         </Button>
       </div>
     </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,42 +4,44 @@ const routes = [
     path: '/wallet',
     name: 'Wallet',
     icon: 'wallet',
+    children: [
+      {
+        id: 2,
+        path: '/all-coins',
+        name: 'All coins',
+        parent: 1,
+      },
+      {
+        id: 3,
+        path: '/borrow',
+        name: 'Borrow',
+        parent: 1,
+      },
+      {
+        id: 4,
+        path: '/contacts',
+        name: 'Contacts',
+        parent: 1,
+      },
+      {
+        id: 5,
+        path: '/settings',
+        name: 'Settings',
+        parent: 1,
+      },
+    ],
   },
   {
-    id: 2,
+    id: 6,
     path: '/crowdsale',
     name: 'Crowdsale',
     icon: 'coin',
   },
   {
-    id: 3,
+    id: 7,
     path: '/special-offers',
     name: 'Special Offers',
     icon: 'tag',
-  },
-  {
-    id: 4,
-    path: '/all-coins',
-    name: 'All coins',
-    parent: 1,
-  },
-  {
-    id: 5,
-    path: '/borrow',
-    name: 'Borrow',
-    parent: 1,
-  },
-  {
-    id: 6,
-    path: '/contacts',
-    name: 'Contacts',
-    parent: 1,
-  },
-  {
-    id: 7,
-    path: '/settings',
-    name: 'Settings',
-    parent: 1,
   },
 ];
 
