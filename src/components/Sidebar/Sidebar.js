d53c10e9a024d4c5d1cc41102d284f5e2eb71e89
Paulina Masiak <paula.masiak@gmail.com>
2018-09-13 15:40:31 +0200

    Update Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Drawer from '../Drawer';
+import Button from '../Button';
+import routes from '../../routes';
 
 import './Sidebar.css';
 
 const Sidebar = (props) => (
   <Drawer show={props.show}>
-    <div className="Sidebar" />
+    <div className="Sidebar">
+      <div className="Sidebar__body" />
+      <div className="Sidebar__footer">
+        <Button color="secondary" block>
+          Sing out
+        </Button>
+      </div>
+    </div>
   </Drawer>
 );
 
