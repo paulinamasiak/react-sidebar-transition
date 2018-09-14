677efad46a6488114cd94d18006641a1f3773e17
Paulina Masiak <paula.masiak@gmail.com>
2018-09-14 12:46:34 +0200

    Fix List component test

--- src/components/List/__test__/List.test.js
+++ src/components/List/__test__/List.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from '../Drawer';
+import List from '../List';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
